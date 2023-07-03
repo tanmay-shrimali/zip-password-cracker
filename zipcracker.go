327f7c3f4fece868ff0eaf5b3c58d84a458e1c6e
Tanmay Shrimali <119653072+tanmay-shrimali@users.noreply.github.com>
2023-07-03 21:42:51 +0530

    Add files via upload

---
+++ zipcracker.go
@@ -0,0 +1,50 @@
+﻿package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
+
+func crackZipPassword(zipFile, password string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	r, err := zip.OpenReader(zipFile)
+	if err != nil {
+		fmt.Printf("Error opening zip file: %s\n", err)
+		return
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		f.SetPassword(password)
+		rc, err := f.Open()
+		if err == nil {
+			// Password successfully cracked!
+			fmt.Printf("Password found: %s\n", password)
+			defer rc.Close()
+			break
+		}
+	}
+}
+
+func main() {
+	zipFile := "file.zip" // Replace with the name of your zip file
+	passwords := []string{
+		"password1",
+		"password2",
+		"password3",
+	} // Add more passwords to try
+
+	var wg sync.WaitGroup
+	for _, password := range passwords {
+		wg.Add(1)
+		go crackZipPassword(zipFile, password, &wg)
+	}
+
+	wg.Wait()
+	fmt.Println("Password cracking completed.")
+}
+
